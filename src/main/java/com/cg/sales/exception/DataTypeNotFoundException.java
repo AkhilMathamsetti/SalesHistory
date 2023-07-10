f41cbb942736cdca69a85b9dfdc9d2a9aab5996d
mssnvgpa <mssnvgpa@INLEN8520041992.corp.capgemini.com>
2023-07-10 08:58:49 +0530

    commit

---
+++ src/main/java/com/cg/sales/exception/DataTypeNotFoundException.java
@@ -0,0 +1,18 @@
+package com.cg.sales.exception;
+
+public class DataTypeNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DataTypeNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public DataTypeNotFoundException(String msg) {
+		// TODO Auto-generated constructor stub
+		super(msg);
+	}
+}
