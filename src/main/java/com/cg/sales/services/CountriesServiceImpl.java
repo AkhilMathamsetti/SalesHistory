40e77e5b95c58882d1cd6e40b6408836e19719cc
MSSNVGPA <MSSNVGPA@INLEN8520041992.corp.capgemini.com>
2023-07-13 16:20:15 +0530

    Countries

--- src/main/java/com/cg/sales/services/CountriesServiceImpl.java
+++ src/main/java/com/cg/sales/services/CountriesServiceImpl.java
@@ -40,10 +40,8 @@ public class CountriesServiceImpl implements CountriesService {
 
 	@Override
 	public Countries updateCountry(Integer countryId,Countries country) {
-		Countries existingCountry = getCountry(countryId);
-		existingCountry = country;
-		//Countries countries = new Countries(country.getCountryId(),country.getCountryIsoCode(),country.getCountryName(),country.getCountryRegion(),country.getCountryRegionId(),country.getCountrySubregion(),country.getCountrySubregionId(),country.getCountryTotal(),country.getCountryTotalId());
-		return countriesRepository.save(existingCountry);
+		Countries countries = new Countries(country.getCountryId(),country.getCountryIsoCode(),country.getCountryName(),country.getCountryRegion(),country.getCountryRegionId(),country.getCountrySubregion(),country.getCountrySubregionId(),country.getCountryTotal(),country.getCountryTotalId());
+		return countriesRepository.save(countries);
 	}
 
 	@Override
